e6023260fb46dbb3eaaaa0c39a300fe213b03c6b
Edyane <edyanealves@gmail.com>
2023-01-03 22:07:11 -0300

    Ajustando projeto

---
+++ learning/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from 'reactstrap';
+import './header.css';
+
+const Header = () => {
+  return (
+    <section>
+        <container>
+            <div className="navigation">
+                <h2>Learners</h2>
+            </div>
+        </container>
+    </section>
+  )
+}
+
+export default Header
