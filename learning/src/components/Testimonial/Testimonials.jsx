c0c358193715cc09b356d5da71e3ea9bcf116432
Edyane <edyanealves@gmail.com>
2023-01-20 20:40:22 -0300

    Adicionando react-slick ao projeto

--- learning/src/components/Testimonial/Testimonials.jsx
+++ learning/src/components/Testimonial/Testimonials.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./testimonial.css";
 import { Container, Row, Col } from "reactstrap";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css"
 
 import img from "../../assets/images/testimonial01.png";
 
