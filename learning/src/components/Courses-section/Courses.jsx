55edc697095f737b00f682d97ffea63b3f6a0aca
Edyane <edyanealves@gmail.com>
2023-01-26 20:48:39 -0300

    Alterando cores do template

--- learning/src/components/Courses-section/Courses.jsx
+++ learning/src/components/Courses-section/Courses.jsx
@@ -43,7 +43,7 @@ const Courses = () => {
           <Col lg="12" className="mb-5">
             <div className="course__top d-flex justify-content-between align-items-center">
               <div className="course__top__left w-50">
-                <h2 className="title">Our Popular Courses</h2>
+                <h2>Our Popular Courses</h2>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
                   consequatur libero quod voluptatibus ullam quia quas, vitae
